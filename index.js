a316adfe4753a6561bc4047729253f5e0f671681
Saloni <saloniaggarwalofficial339@gmail.com>
2023-07-30 21:55:30 +0530

    project

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
- //creating express server
- const express = require('express');
- const dotenv = require('dotenv').config();
- const port = process.env.PORT || 5000;
- const app = express();
- //enable body parser
- app.use(express.json());
- app.use(express.urlencoded({extended : false}));
-  //connecting this with openai_routes.js file
-  app.use('/openai', require('./routes/openaiRoutes'));
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
 
-  app.listen(port, () => console.log(`Server started on port ${port}`));
+const app = express();
+
+// Enable body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/openai', require('./routes/openaiRoutes'));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
